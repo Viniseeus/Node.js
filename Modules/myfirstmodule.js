ae695ba12270d2495dfa759d480620b8ae103772
Viniseeus <vinigolarte@gmail.com>
2023-04-23 20:39:41 -0300

    Atualizações

---
+++ Modules/myfirstmodule.js
@@ -0,0 +1,38 @@
+var http = require('http');
+var dt = require('./myfirstmodule');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("The date and time are currently: " + dt.myDateTime());
+  res.end();
+}).listen(8080);
+
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
+
+//http://localhost:8080/summer// Link que irá printar "summer" na tela
+//http://localhost:8080/winter// Link que irá printar "winter" na tela
+
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+  res.end(txt);
+}).listen(8080);
+
+//http://localhost:8080/?year=2017&month=July//
